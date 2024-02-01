b4a6541fe974319d2f6620b42250d88261b81da8
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2024-02-01 16:41:20 +0100

    Build the first homework (Test OK)

---
+++ Devoirs/Integrate/build/CMakeFiles/myFem.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myFem.dir/src/fem.c.o"
+  "CMakeFiles/myFem.dir/src/fem.c.o.d"
+  "CMakeFiles/myFem.dir/src/glfem.c.o"
+  "CMakeFiles/myFem.dir/src/glfem.c.o.d"
+  "CMakeFiles/myFem.dir/src/homework.c.o"
+  "CMakeFiles/myFem.dir/src/homework.c.o.d"
+  "CMakeFiles/myFem.dir/src/main.c.o"
+  "CMakeFiles/myFem.dir/src/main.c.o.d"
+  "myFem"
+  "myFem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myFem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
