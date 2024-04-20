c3089a2648fca7c18b2d3fefe7f092713ca0d78a
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2024-04-20 20:01:47 +0200

    Fix Bugs POST

--- Project/ProjectPostProcssor/src/convergence_plot.py
+++ Project/ProjectPostProcssor/src/convergence_plot.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# !! LOST THE DATAS !!
+# Forgot to push the csv file to the repo, and then "git stash", rest in peace "deformation_beam.csv" :(
+# This data is false because we did some error in the calculation of the error in the code
+# Check the real graph in the rapport (with the real data)
+
 data = np.genfromtxt('../../Rapport/data/csv/deformation.csv', delimiter=',', skip_header=1)
 
 h = data[:, 0]
@@ -16,17 +21,19 @@ ref_error_total = error_total[0]
 error_total = error_total[1:]
 h = h[1:]
 
+# error_total = (ref_error_total - error_total) * (ref_error_total - error_total)
 error_total = (ref_error_total - error_total) * (ref_error_total - error_total)
 
 plt.figure()
-plt.plot(h, error_total, marker='o', linestyle='-', color='b')
-plt.gca().invert_xaxis()
+plt.plot(h, error_total, marker='o', linestyle='-', color='b', label='Experimental Convergence Error')
+plt.plot(h, (h**2 / 50000)**2, linestyle='--', color='r', label='Convergence Order: 2')
+plt.plot(h, (h**0.75 / 80000)**2, linestyle='--', color='g', label='Convergence Order: 0.75')
 plt.xlabel('Mesh Size ($h$)')
 plt.ylabel('Convergence Error')
 plt.title('Convergence Error vs. Mesh Size')
 plt.grid(True)
 plt.xscale("log")
 plt.yscale("log")
+plt.legend(loc='upper left', prop={'size': 10})
 # plt.savefig('../../Rapport/data/convergence_plot.pdf', bbox_inches='tight')
-plt.legend(['Experimental Convergence Error'], loc='upper right', prop={'size': 10})
 plt.show()
